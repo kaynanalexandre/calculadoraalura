4664958c716965ca075536c6cbb9a0d49d8d5e8c
kaynanalexandre <kaynaalexandre1412@gmail.com>
2024-08-19 19:46:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿function init(num){ ///adiciona os números no campo
+    let number = document.getElementById("camp").innerHTML;
+    document.getElementById("camp").innerHTML = number + num
+}
+function limpar(){/// retira o número do campo
+    var number = document.getElementById("camp").innerHTML;
+    document.getElementById('camp').innerHTML = number.substring(0, number.length -1) 
+}
+function calc() {///calcula o campo
+    camp = document.getElementById("camp").innerHTML;
+    document.getElementById("camp").innerHTML = eval(camp)
+}
+window.alert('calculadora teste alura"')
